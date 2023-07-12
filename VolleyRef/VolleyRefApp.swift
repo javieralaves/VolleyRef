8db7b8933da6fa0cbe6ed0bfc18863f62ad5e0a4
Javier Alaves <jayalaves@gmail.com>
2023-07-12 11:41:10 +0200

    Initial Commit

---
+++ VolleyRef/VolleyRefApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyRefApp.swift
+//  VolleyRef
+//
+//  Created by Javier Alaves on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct VolleyRefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
