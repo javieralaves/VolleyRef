cfc67f243413109117a81bb1e6598505a74c5c10
Javier Alaves <jayalaves@gmail.com>
2023-07-12 12:33:40 +0200

    Switch sides alert

--- VolleyRef/ContentView.swift
+++ VolleyRef/ContentView.swift
@@ -8,15 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var teamOneScore: Int = 0
+    @State var teamTwoScore: Int = 0
+    
+    @State var switchAlert: Bool = false
+    @State var winAlert: Bool = false
+    
+    @State var matchWinner: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Spacer()
+            VStack (spacing: 20) {
+                Text("Team 1")
+                
+                Button {
+                    teamOneScore += 1
+                    switchSides()
+                } label: {
+                    Text("\(teamOneScore)")
+                        .font(.largeTitle)
+                        .frame(width: 120, height: 120)
+                        .background(Color.blue.opacity(0.1))
+                        .cornerRadius(.infinity)
+                }
+            }
+            Spacer()
+            VStack (spacing: 20) {
+                Text("Team 2")
+                
+                Button {
+                    teamTwoScore += 1
+                    switchSides()
+                } label: {
+                    Text("\(teamTwoScore)")
+                        .font(.largeTitle)
+                        .frame(width: 120, height: 120)
+                        .background(Color.blue.opacity(0.1))
+                        .cornerRadius(.infinity)
+                }
+            }
+            Spacer()
         }
-        .padding()
+        .alert(isPresented: $switchAlert) {
+            Alert(title: Text("Switch sides"),
+                  message: Text("It's time to switch sides"),
+                  dismissButton: .default(Text("OK")))
+        }
+
     }
+    
+    func switchSides() {
+        let scoreSum = teamOneScore + teamTwoScore
+        
+        if scoreSum.isMultiple(of: 7) {
+            switchAlert = true
+            print("Switch sides")
+        } else {
+            switchAlert = false
+        }
+    }
+    
+    func matchEnd() {
+        
+        if teamOneScore == 21 {
+            matchWinner = "Team 1"
+            print("Team 1 won")
+            winAlert = true
+        } else if teamTwoScore == 21 {
+            matchWinner = "Team 2"
+            print("Team 2 won")
+            winAlert = true
+        } else {
+            matchWinner = ""
+            print("Match ongoing")
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
